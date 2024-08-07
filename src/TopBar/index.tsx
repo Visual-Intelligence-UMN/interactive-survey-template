50009f49f6bf67250f54005c4a523b0b2588a6c0
wangqianwen0418 <wangqianwen0418@163.com>
2024-08-06 21:15:39 -0500

    change github to homepage

--- src/TopBar/index.tsx
+++ src/TopBar/index.tsx
@@ -1,77 +1,77 @@
 import React from "react";
 import {
-  Typography,
-  Toolbar,
-  IconButton,
-  AppBar,
-  Badge,
-  InputBase,
+    Typography,
+    Toolbar,
+    IconButton,
+    AppBar,
+    Badge,
+    InputBase,
 } from "@material-ui/core";
 import {
-  // AccountCircle,
-  // Notifications as NotificationsIcon,
-  // Mail as MailIcon,
-  Menu as MenuIcon,
-  // Search as SearchIcon,
-  CloudUpload,
-  Description,
-  GitHub
+    // AccountCircle,
+    // Notifications as NotificationsIcon,
+    // Mail as MailIcon,
+    Menu as MenuIcon,
+    // Search as SearchIcon,
+    CloudUpload,
+    Description,
+    Home
 } from "@material-ui/icons";
 
 import { useStyles } from "./style";
 
 interface Props {
-  title: string;
-  preprint: string;
-  github: string;
-  topTheme: string;
-  handleDrawerToggle: () => void;
-  onProfileMenuOpen: (e: React.MouseEvent<HTMLElement>) => void;
+    title: string;
+    preprint: string;
+    github: string;
+    topTheme: string;
+    handleDrawerToggle: () => void;
+    onProfileMenuOpen: (e: React.MouseEvent<HTMLElement>) => void;
 }
 
 export function TopBar(props: Props) {
-  const classes = useStyles();
-  const {title, preprint, github, topTheme, onProfileMenuOpen, handleDrawerToggle } = props;
-  console.log("preprint here: ", preprint)
+    const classes = useStyles();
+    const { title, preprint, github, topTheme, onProfileMenuOpen, handleDrawerToggle } = props;
+    console.log("preprint here: ", preprint)
 
-  return (
-    <AppBar position="fixed" className={classes.appBar}>
-      <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="start"
-          onClick={handleDrawerToggle}
-          className={classes.menuButton}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" noWrap className={classes.title}>
-          {title}
-        </Typography>
+    return (
+        <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    edge="start"
+                    onClick={handleDrawerToggle}
+                    className={classes.menuButton}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" noWrap className={classes.title}>
+                    {title}
+                </Typography>
 
-        <div className={classes.sectionDesktop}>
-          <IconButton
-            edge="end"
-            aria-label="link to arxiv paper"
-            aria-haspopup="true"
-            onClick={()=>window.open(preprint)}
-            color="inherit"
-            disabled={!preprint}
-          >
-            <Description /> <span style={{fontSize: '12px'}} className={classes.iconName}>{' '}Preprint </span>
-          </IconButton>
-          <IconButton
-            edge="end"
-            aria-label="link to github homepage"
-            aria-haspopup="true"
-            onClick={()=>window.open(github)}
-            color="inherit"
-            disabled={!github}
-          >
-            <GitHub /> <span style={{fontSize: '12px'}} className={classes.iconName}>{' '}Github </span>
-          </IconButton>
-          {/* <IconButton
+                <div className={classes.sectionDesktop}>
+                    <IconButton
+                        edge="end"
+                        aria-label="link to arxiv paper"
+                        aria-haspopup="true"
+                        onClick={() => window.open(preprint)}
+                        color="inherit"
+                        disabled={!preprint}
+                    >
+                        <Description /> <span style={{ fontSize: '12px' }} className={classes.iconName}>{' '}Preprint </span>
+                    </IconButton>
+                    <IconButton
+                        edge="end"
+                        aria-label="link to github homepage"
+                        aria-haspopup="true"
+                        onClick={() => window.open(github)}
+                        color="inherit"
+                        disabled={!github}
+                    >
+                        <Home /> <span style={{ fontSize: '12px' }} className={classes.iconName}>{' '}Homepage </span>
+                    </IconButton>
+                    {/* <IconButton
             edge="end"
             aria-label="suggest new ML4VIS papers for this survey"
             aria-haspopup="true"
@@ -80,8 +80,8 @@ export function TopBar(props: Props) {
           >
             <CloudUpload />  <span style={{fontSize: '12px'}} className={classes.iconName}> {' '}Contribute </span>
           </IconButton> */}
-        </div>
-      </Toolbar>
-    </AppBar>
-  );
+                </div>
+            </Toolbar>
+        </AppBar>
+    );
 }
